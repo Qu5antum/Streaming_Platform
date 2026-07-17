9ef407a8d7a59250cd4315f85d02ff106a0b1147
Qu5antum <erae88959@gmail.com>
2026-07-17 15:12:53 +0500

    websocket service init

---
+++ backend/src/websocket_server/websocket_service.py
@@ -0,0 +1,22 @@
+class WebSocketService:
+
+    async def authenticate():
+        pass
+
+    async def connect():
+        pass
+
+    async def send_history():
+        pass
+
+    async def send_viewers_count():
+        pass
+
+    async def process_message():
+        pass
+
+    async def disconnect():
+        pass
+
+    async def chat():
+        pass
