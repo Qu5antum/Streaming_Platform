e7b6ab470005e0124b697d108b3ed857d353501a
Qu5antum <erae88959@gmail.com>
2026-07-17 23:07:01 +0500

    WebSocket service update init

--- backend/src/websocket_server/websocket_service.py
+++ backend/src/websocket_server/websocket_service.py
@@ -1,12 +1,13 @@
 from fastapi import WebSocketDisconnect, WebSocket
 from uuid import UUID
-from websockets.asyncio.server import ServerConnection
 import logging
+from datetime import datetime, timezone
 
-from src.database.db import AsyncSession
 from src.services.stream_message_service import StreamMessageService
+from src.services.stream_metric_serivce import StreamMetricService
 from .websocket_conection_manager import ConnectionManager
-from src.api.schemas.stream_message_schema import MessageResponse, MessageRequest
+from src.api.schemas.stream_message_schema import MessageRequest
+from src.database.models import User
 
 logger = logging.getLogger("websocket_service")
 
@@ -16,21 +17,40 @@ class WebSocketService:
         self,
         websocket: WebSocket,
         stream_id: UUID,
-        message_service: StreamMessageService,
         manager: ConnectionManager,
+        message_service: StreamMessageService,
+        metric_service: StreamMetricService,
     ):
         self.websocket = websocket
         self.stream_id = stream_id
-        self.message_service = message_service
         self.manager = manager
+        self.message_service = message_service
+        self.metric_service = metric_service
 
-        self.user = None 
+        self.user: User | None = None
+        self.connected_at: datetime | None = None
 
     async def authenticate(self):
         self.user = None # add jwt autorization
 
     async def connect(self):
-        pass
+        await self.authenticate()
+
+        await self.manager.connect(
+            stream_id=self.stream_id,
+            websocket=self.websocket,
+            user=self.user,
+        )
+
+        self.connected_at = datetime.now(timezone.utc)
+
+        await self.metric_service.viewer_connected(
+            self.stream_id
+        )
+
+        await self.metric_service.register_view(
+            self.stream_id
+        )
 
     async def send_history(self):
         messages = await self.message_service.get_messages_by_stream_id(
@@ -48,15 +68,18 @@ class WebSocketService:
         )
 
     async def send_viewers_count(self):
-        await self.websocket.send_json(
+        metrics = await self.metric_service.get_live_stream_metrics(
+            self.stream_id
+        )
+
+        await self.manager.broadcast(
+            self.stream_id,
             {
                 "type": "viewer_count",
                 "data": {
-                    "count": self.manager.get_viewers_count(
-                        self.stream_id
-                    )
+                    "count": metrics["current_viewers"]
                 },
-            }
+            },
         )
 
     async def process_message(self, payload):
@@ -80,32 +103,34 @@ class WebSocketService:
             self.websocket,
         )
 
+        await self.metric_service.viewer_discconnected(
+            self.stream_id,
+        )
+
+        if self.connected_at:
+
+            seconds = int(
+                (
+                    datetime.now(timezone.utc)
+                    - self.connected_at
+                ).total_seconds()
+            )
+
+            await self.metric_service.register_watch_session(
+                self.stream_id,
+                seconds,
+            )
+
     async def chat(self):
         await self.connect()
-
         await self.send_history()
-
         await self.send_viewers_count()
-
-        await self.manager.connect(self.stream_id, self.websocket)
 
         try:
             while True:
                 payload = await self.websocket.receive_json()
 
-                message = await self.message_service.add_new_message(
-                    stream_id=self.stream_id,
-                    user=self.user,
-                    message=MessageRequest(**payload),
-                )
-
-                await self.manager.broadcast(
-                    self.stream_id,
-                    {
-                        "type": "chat_message",
-                        "data": MessageResponse.model_validate(message).model_dump(mode="json"),
-                    },
-                )
+                await self.process_message(payload)
 
         except WebSocketDisconnect:
             logger.info(
@@ -121,3 +146,4 @@ class WebSocketService:
 
         finally:
             await self.manager.disconnect(self.stream_id, self.websocket)
+            await self.send_viewers_count()
